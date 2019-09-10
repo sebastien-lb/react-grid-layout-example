08a82f8168788aabc12365a98242babb14efcca5
sebastien-lb <sebast.lubineau@laposte.net>
2019-09-10 13:55:46 +0200

    add pages skeleton

---
+++ src/pages/LayoutWithAutoSave/LayoutWithAutoSave.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { withStyles } from "@material-ui/core/styles";
+import { Style } from "jss";
+import { CSSProperties } from "@material-ui/styles";
+
+type ClassNames = "container";
+interface OwnProps {
+  classes: Record<ClassNames, string>;
+}
+
+type Props = OwnProps;
+
+export class LayoutWithAutoSave extends React.Component<Props> {
+  public render(): JSX.Element {
+    return <div></div>;
+  }
+}
+
+const styles: Style = (): Record<ClassNames, CSSProperties> => ({
+  container: {}
+});
+
+export default withStyles(styles)(LayoutWithAutoSave);
---
+++ src/pages/index.ts
@@ -0,0 +1,5 @@
+export { default as BasicLayout } from "./BasicLayout/BasicLayout";
+export { default as LayoutWithSave } from "./LayoutWithSave/LayoutWithSave";
+export {
+  default as LayoutWithAutoSave
+} from "./LayoutWithAutoSave/LayoutWithAutoSave";
